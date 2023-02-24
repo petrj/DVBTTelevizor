3dc94ebf31f1dafdc0cbc5787202cede95076122
Petr Janoušek <pjanousek@lekis.cz>
2023-02-24 09:06:42 +0100

    PushTestingDVBTDriverDataToDevice.bat

---
+++ PushTestingDVBTDriverDataToDevice.bat
@@ -0,0 +1,10 @@
+@cd /d %~dp0
+cd TestingDVBTDriverManager.DebugData
+
+set androidFolder=/storage/emulated/0/Android/media/net.petrjanousek.DVBTTelevizor/
+
+adb push stream.ts %androidFolder%
+adb push 310.ts %androidFolder%
+adb push 2300.ts %androidFolder%
+adb push 2400.ts %androidFolder%
+adb push 7070.ts %androidFolder%
