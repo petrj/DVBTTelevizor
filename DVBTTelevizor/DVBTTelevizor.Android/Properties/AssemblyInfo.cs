dcbc30889ae8ca4df086fd533929e72a8244e431
Petr Janoušek <pjanousek@lekis.cz>
2023-08-15 11:29:30 +0200

    Version 14

--- DVBTTelevizor/DVBTTelevizor.Android/Properties/AssemblyInfo.cs
+++ DVBTTelevizor/DVBTTelevizor.Android/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("13.0.0.0")]
-[assembly: AssemblyFileVersion("13.0.0.0")]
+[assembly: AssemblyVersion("14.0.0.0")]
+[assembly: AssemblyFileVersion("14.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
