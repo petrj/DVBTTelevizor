351ef6df4c35e3572bc168ac5d7f25efd5e8f909
Petr Janoušek <pjanousek@lekis.cz>
2024-03-20 09:01:49 +0100

    ver 35 (Android TV)

--- DVBTTelevizor/DVBTTelevizor.Android/Properties/AssemblyInfo.cs
+++ DVBTTelevizor/DVBTTelevizor.Android/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("34.0.0.0")]
-[assembly: AssemblyFileVersion("34.0.0.0")]
+[assembly: AssemblyVersion("35.0.0.0")]
+[assembly: AssemblyFileVersion("35.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
