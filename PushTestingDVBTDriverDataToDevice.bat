ceac5475f793a456c82c180b2bff11febd1521f1
Petr Janoušek <petrjanousek.net@gmail.com>
2024-03-21 18:17:32 +0100

    banner/icon

--- PushTestingDVBTDriverDataToDevice.bat
+++ PushTestingDVBTDriverDataToDevice.bat
@@ -3,4 +3,7 @@ cd TestingDVBTDriverData
 
 set androidFolder=/storage/emulated/0/Android/media/net.petrjanousek.DVBTTelevizor/
 
-adb push DVBT-MPEGTS-514MHz-2023-08-15-23-13-38.ts %androidFolder%
+adb push DVBT-MPEGTS-474MHz-2024-03-12-14-32-36.ts %androidFolder%
+adb push DVBT-MPEGTS-490MHz-2024-03-12-16-06-02.ts %androidFolder%
+adb push DVBT-MPEGTS-530MHz-2024-03-11-20-50-27.ts %androidFolder%
+adb push DVBT-MPEGTS-626MHz-2024-03-11-21-08-27.ts %androidFolder%
--- PushTestingDVBTDriverManagerDataToDevice.bat
+++ PushTestingDVBTDriverManagerDataToDevice.bat
@@ -3,8 +3,4 @@ cd TestingDVBTDriverManagerData
 
 set androidFolder=/storage/emulated/0/Android/media/net.petrjanousek.DVBTTelevizor/
 
-adb push stream.ts %androidFolder%
-adb push 310.ts %androidFolder%
-adb push 2300.ts %androidFolder%
-adb push 2400.ts %androidFolder%
-adb push 7070.ts %androidFolder%
+adb push *.* %androidFolder%
