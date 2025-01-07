d98671f3fbabfc1addf15a2f94111bf78b56c39f
petrj <petrjanousek.net@gmail.com>
2025-01-07 18:16:52 +0100

    MAUI Project files

---
+++ DVBTTelevizor.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DVBTTelevizor.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
