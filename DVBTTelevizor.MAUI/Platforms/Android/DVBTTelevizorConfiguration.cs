56d66ee6d822e0195226ab3e12e918b005f846b4
petrj <petrjanousek.net@gmail.com>
2025-01-25 00:23:54 +0100

    Android Configuration

---
+++ DVBTTelevizor.MAUI/Platforms/Android/DVBTTelevizorConfiguration.cs
@@ -0,0 +1,144 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using LoggerService;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVBTTelevizor.MAUI
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    internal class DVBTTelevizorConfiguration : CustomSharedPreferencesObject, ITVCConfiguration
+    {
+        private ILoggingService _loggingService;
+        private string _configDirectory = string.Empty;
+
+        public bool Fullscreen
+        {
+            get
+            {
+                return GetPersistingSettingValue<bool>("Fullscreen");
+            }
+            set
+            {
+                SavePersistingSettingValue<bool>("Fullscreen", value);
+            }
+        }
+
+        public DVBTTelevizorConfiguration(ILoggingProvider loggingProvider, IPublicDirectoryProvider publicDirectoryProvider)
+        {
+            if (loggingProvider != null)
+            {
+                _loggingService = loggingProvider.GetLoggingService();
+            } else
+            {
+                _loggingService = new BasicLoggingService();
+            }
+
+            if (publicDirectoryProvider != null)
+            {
+                _configDirectory = publicDirectoryProvider.GetPublicDirectoryPath();
+            }
+        }
+
+        public ObservableCollection<Channel> Channels { get; set; } = new ObservableCollection<Channel>();
+
+        private string ChannelsConfigFileName
+        {
+            get
+            {
+                return Path.Join(ConfigDirectory, "DVBTTelevizor.MAUI.channels.json");
+            }
+        }
+
+        public string ConfigDirectory
+        {
+            get
+            {
+                return _configDirectory;
+            }
+            set
+            {
+                _configDirectory = value;
+            }
+        }
+
+        public int ImportChannelsFromJSON(string json)
+        {
+            try
+            {
+                var importedChannels = JsonConvert.DeserializeObject<ObservableCollection<Channel>>(json);
+
+                var count = 0;
+                foreach (var ch in importedChannels)
+                {
+                    if (!ch.ChannelExists(Channels))
+                    {
+                        count++;
+                        ch.Number = Channel.GetNextChannelNumber(Channels).ToString();
+                        Channels.Add(ch);
+                    }
+                }
+
+                return count;
+            }
+            catch (Exception ex)
+            {
+                _loggingService.Error(ex, "Import failed");
+                return -1;
+            }
+        }
+
+        public void Load()
+        {
+            try
+            {
+                var json = GetPersistingSettingValue<string>("ChannelsJson");
+                if (string.IsNullOrEmpty(json) && (File.Exists(ChannelsConfigFileName)))
+                {
+                    json = File.ReadAllText(ChannelsConfigFileName);
+                }
+
+                if (!string.IsNullOrEmpty(json))
+                {
+                    var loadedChannels = JsonConvert.DeserializeObject<ObservableCollection<Channel>>(json);
+
+                    if (loadedChannels != null && loadedChannels.Count > 0)
+                    {
+                        Channels.Clear();
+
+                        foreach (var channel in loadedChannels)
+                        {
+                            Channels.Add(channel.Clone());
+                        }
+                    }
+                }
+
+            } catch (Exception ex)
+            {
+                _loggingService.Error(ex);
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _loggingService.Info("Saving channels");
+
+                var json = JsonConvert.SerializeObject(Channels);
+
+                SavePersistingSettingValue<string>("ChannelsJson", JsonConvert.SerializeObject(Channels));
+
+                File.WriteAllText(ChannelsConfigFileName, json);
+            }
+            catch (Exception ex)
+            {
+                _loggingService.Error(ex);
+            }
+        }
+    }
+}
