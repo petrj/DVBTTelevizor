39bf213cd2e0505c162d7faa75fe69e17612c267
petrj <petrjanousek.net@gmail.com>
2025-03-10 23:20:29 +0100

    DVBTDriverChangedMessage

---
+++ DVBTTelevizor.MAUI/Messages/DVBTDriverChangedMessage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVBTTelevizor.MAUI.Messages
+{
+    internal class DVBTDriverChangedMessage : ValueChangedMessage<string>
+    {
+        public DVBTDriverChangedMessage(string value) : base(value)
+        {
+
+        }
+    }
+}
