78a59d93c2106ce0e729c9983e3f7c3883e24416
petrj <petrjanousek.et@gmail.com>
2025-06-04 23:07:38 +0200

    linux build script

--- BuildMAUILinux.ps1
+++ BuildMAUILinux.ps1
@@ -1,1 +1,3 @@
-dotnet build /p:AndroidSdkDirectory=/home/kirk/Android/Sdk/ /t:Run -p:AndroidEmulator=true DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj 
+#dotnet build /p:AndroidSdkDirectory=/home/kirk/Android/Sdk/ /t:Run -p:AndroidEmulator=true DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj 
+dotnet build /p:AndroidSdkDirectory=/home/kirk/Android/Sdk/ /t:Build DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj 
+
