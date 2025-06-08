e32c349de179291707e4d36c31aaa7eeaf98c93d
petrj <petrjanousek.net@gmail.com>
2025-06-08 13:48:53 +0200

    Build MAUI Windows PS

--- BuildMAUIWindows.ps1
+++ BuildMAUIWindows.ps1
@@ -1,27 +1,82 @@
-param (
-    [ValidateSet("net9.0-windows10.0.22000.0", "net9.0-android35.0")]
-    [string]$framework,
+param ([string]$framework,[string]$configuration)
 
-    [ValidateSet('Release', 'Debug')]
-    [string]$configuration
-)
+$frameworks = @("net9.0-windows10.0.22000.0", "net9.0-android35.0")
+$configurations = @("Release", "Debug")
 
 #########################################################################################################################
 
+function Get-Value
+{
+    Param
+    (       
+        $Title,
+        $Values
+    )
+    Process
+    {
+        Write-Host $Title
+        $i = 1
+        foreach ($value in $Values)
+        {
+            Write-Host ($i.TosTring() + ") " + $value)
+            $i++
+        }
+
+        $v = Read-Host
+        $num = 0
+
+        if (-not ([int]::TryParse($v, [ref] $num)))
+        {
+            throw "Invalid value"
+        } 
+
+        if (($num -lt 1) -or ($num -gt $Values.Count))
+        {
+            throw "Invalid value"
+        }
+        
+        return ($Values[$num-1])
+    }
+}
+
 if ([String]::IsNullOrWhiteSpace($framework))
 {
-    $framework =  (
-        $MyInvocation.MyCommand.Parameters['framework'].Attributes |
-            Where-Object { $_ -is [System.Management.Automation.ValidateSetAttribute] }
-        ).ValidValues[0]
+    $framework =  $frameworks[0]
 }
 
 if ([String]::IsNullOrWhiteSpace($configuration))
 {
-    $configuration = (
-        $MyInvocation.MyCommand.Parameters['configuration'].Attributes |
-            Where-Object { $_ -is [System.Management.Automation.ValidateSetAttribute] }
-        ).ValidValues[0]
+    $configuration = $configurations[0]
+}
+
+if ($framework -eq "?")
+{
+    $framework = Get-Value -Title "Set framework:" -Values $frameworks
+}
+
+if ($configuration -eq "?")
+{
+    $configuration = Get-Value -Title "Set configuration:" -Values $configurations
+}
+
+if (-not ($configurations.Contains($configuration)))
+{
+    $configuration = $configurations | Where-Object { $_ -like "*$configuration*"} 
+}
+
+if (-not ($configurations.Contains($configuration)))
+{
+    throw "Invalid configuration"
+}
+
+if (-not ($frameworks.Contains($framework)))
+{
+    $framework = $frameworks | Where-Object { $_ -like "*$framework*"}
+}
+
+if (-not ($frameworks.Contains($framework)))
+{
+    throw "Invalid framework"
 }
 
 #########################################################################################################################
