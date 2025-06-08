d9eccfd3e365070f16a360b0f7d1fbbfbcebea8d
petrj <petrjanousek.et@gmail.com>
2025-06-08 13:06:08 +0200

    PS run in emulator script (not working)

--- BuildMAUILinux.ps1
+++ BuildMAUILinux.ps1
@@ -1,3 +1,4 @@
 #dotnet build /p:AndroidSdkDirectory=/home/kirk/Android/Sdk/ /t:Run -p:AndroidEmulator=true DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj 
+./Clear.ps1
 dotnet build /p:AndroidSdkDirectory=/home/kirk/Android/Sdk/ /t:Build DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj 
 
