3d686bb3d363db3e69b856c55c01971e5a39ef97
petrj <petrjanousek.net@gmail.com>
2025-08-30 10:07:00 +0200

    Version 2025001 for Google Play, built by MAUI-Publish-Bundle.ps1

--- MAUI-Publish-Bundle.ps1
+++ MAUI-Publish-Bundle.ps1
@@ -3,12 +3,11 @@
 
 dotnet publish .\DVBTTelevizor.MAUI\DVBTTelevizor.MAUI.csproj -c Release -f net9.0-android35.0 /p:AndroidPackageFormat=aab
 
-Write-Host "Signing the package. Enter the password, please"
-
 $jarsigner = "C:\Program Files (x86)\Android\openjdk\jdk-17.0.14\bin\jarsigner.exe"
 $aab = "DVBTTelevizor.MAUI\bin\Release\net9.0-android35.0\net.petrjanousek.DVBTTelevizor.aab"
 $keystore = "C:\Users\petrj\AppData\Local\Xamarin\Mono for Android\KeyStore\PJsAndroidKeyStore\PJsAndroidKeyStore.keystore"
 
+Write-Host "Signing the package. Enter the password, please"
 
 $passwEncrypted = Read-Host -AsSecureString
 $ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwEncrypted)
@@ -19,4 +18,12 @@ $passw = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ptr)
 [xml]$manifest = Get-Content ".\DVBTTelevizor.MAUI\Platforms\Android\AndroidManifest.xml"
 $version = $manifest.manifest.versionName
 
-Copy-Item $aab "net.petrjanousek.DVBTTelevizor.AndroidTV.$version.AndroidTV.aab"
+if ($manifest.manifest.'uses-feature'.name -eq  "android.software.leanback")
+{
+    $suffix = ".AndroidTV"
+} else
+{
+    $suffix = ""
+}
+
+Copy-Item $aab "net.petrjanousek.DVBTTelevizor.AndroidTV.${version}${suffix}.aab"
