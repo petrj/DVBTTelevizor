42efd062f90b20eae024cf99f07b7fafc73a2150
petrj <petrjanousek.net@gmail.com>
2025-08-30 13:42:28 +0200

    Version 2025003 for Google Play, built by MAUI-Publish-Bundle.ps1 invluding apk

--- MAUI-Publish-Bundle.ps1
+++ MAUI-Publish-Bundle.ps1
@@ -17,6 +17,14 @@ DVBT Televizor script for publishing to Google Play
 #>
 
 Set-Location $PSScriptRoot
+#$env:Path += ";C:\Program Files (x86)\Android\android-sdk\platform-tools\"
+
+
+Write-Host "Enter the password to KeyStore, please:" 
+
+$passwEncrypted = Read-Host -AsSecureString
+$ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwEncrypted)
+$passw = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ptr)
 
 # Clear
 
@@ -26,19 +34,16 @@ Set-Location $PSScriptRoot
 
 dotnet publish .\DVBTTelevizor.MAUI\DVBTTelevizor.MAUI.csproj -c Release -f net9.0-android35.0 /p:AndroidPackageFormat=aab
 
-# Sign
+# Sign aab
 
 $jarsigner = "C:\Program Files (x86)\Android\openjdk\jdk-17.0.14\bin\jarsigner.exe"
 $aab = "DVBTTelevizor.MAUI\bin\Release\net9.0-android35.0\net.petrjanousek.DVBTTelevizor.aab"
 $keystore = "C:\Users\petrj\AppData\Local\Xamarin\Mono for Android\KeyStore\PJsAndroidKeyStore\PJsAndroidKeyStore.keystore"
+$keystoreAlias = "PJsAndroidKeyStore"
 
-Write-Host "Signing the package. Enter the password, please"
+Write-Host "Signing the package"
 
-$passwEncrypted = Read-Host -AsSecureString
-$ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwEncrypted)
-$passw = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ptr)
-
-& $jarsigner -keystore $keystore  -storepass $passw $aab "PJsAndroidKeyStore"
+& $jarsigner -keystore $keystore  -storepass $passw $aab $keystoreAlias
 
 Write-Host "Signing the package........."
 
@@ -62,4 +67,40 @@ if ($version -is [System.Array])
     $version = $version[0]
 }
 
-Copy-Item $aab "net.petrjanousek.DVBTTelevizor.${version}${suffix}.aab"
+$aabName = "net.petrjanousek.DVBTTelevizor.${version}${suffix}.aab"
+
+Copy-Item $aab $aabName
+
+# Create APK
+
+    # https://github.com/google/bundletool/releases
+    # "C:\Program Files (x86)\Android\android-sdk\platform-tools\bundletool-all-1.18.1.jar"
+
+$bundleTool = "C:\Program Files (x86)\Android\android-sdk\platform-tools\bundletool-all-1.18.1.jar"
+$java = "C:\Program Files (x86)\Android\openjdk\jdk-17.0.14\bin\java.exe"
+
+$outputArchive = Join-Path -Path $PSScriptRoot -ChildPath "$aabName.apks"
+
+if (Test-Path -Path $outputArchive)
+{
+    Remove-Item -Path $outputArchive -Force -Verbose
+} 
+
+Write-Host "Creating universal APK"
+
+& $java -jar $bundleTool build-apks --bundle=$aabName --output=$outputArchive --mode=universal --ks=$keystore --ks-key-alias=$keystoreAlias --ks-pass=pass:$passw 
+
+
+Rename-Item -Path $outputArchive -NewName ($outputArchive + ".zip")
+$outputArchive+=".zip"
+
+$zip = [System.IO.Compression.ZipFile]::OpenRead($outputArchive)
+$apkName =  [System.IO.Path]::GetFileNameWithoutExtension($aabName) + ".apk"
+
+Expand-Archive -Path $outputArchive -DestinationPath . -Verbose
+
+Remove-Item "*.pb"
+Remove-Item $outputArchive
+Rename-Item "universal.apk" $apkName
+
+Get-Item $apkName
