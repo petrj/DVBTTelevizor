a48d9d880b061ad4929c9cd7e1aec63b378b3d10
PetrJ <petrjanousek.net@gmail.com>
2025-08-31 11:41:45 +0200

    MAUI Powershell Build module

--- MAUI-Publish-Bundle.ps1
+++ MAUI-Publish-Bundle.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 
-DVBT Televizor script for publishing to Google Play
+Script for creating AAB/APK release for publishing to Google Play
 
     Android TV necessary release modifications:
 
@@ -12,92 +12,32 @@ DVBT Televizor script for publishing to Google Play
  
         [IntentFilter(new[] { Intent.ActionMain }, AutoVerify = true, Categories = new[] { Intent.CategoryLeanbackLauncher })]
 
-    Do not include AndroidTV modifications to non-Android TV channel!
+    Do not include AndroidTV modifications to non-Android TV release!
 
 #>
 
 Set-Location $PSScriptRoot
-#$env:Path += ";C:\Program Files (x86)\Android\android-sdk\platform-tools\"
+Import-Module .\MAUI-BuildModule.psm1 -Force
 
-Write-Host "Enter the password to KeyStore, please:" 
-
-$passwEncrypted = Read-Host -AsSecureString
-$ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwEncrypted)
-$passw = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ptr)
-
-# Clear
+$passw = Get-Password
 
 .\Clear.ps1
 
-# Build
+$signedAABPackage = Get-Item ".\DVBTTelevizor.MAUI\DVBTTelevizor.MAUI.csproj" `
+    | Publish-AABPackage `
+        -Configuration Release `
+        -PackageName "net.petrjanousek.DVBTTelevizor" `
+    | Protect-BySignature `
+        -Keystore "C:\Users\petrj\AppData\Local\Xamarin\Mono for Android\KeyStore\PJsAndroidKeyStore\PJsAndroidKeyStore.keystore" `
+        -Alias "PJsAndroidKeyStore" `
+        -Password $passw `
 
-dotnet publish .\DVBTTelevizor.MAUI\DVBTTelevizor.MAUI.csproj -c Release -f net9.0-android35.0 /p:AndroidPackageFormat=aab
+$signedAPKPackage = $signedAABPackage | ConvertTo-APK `
+        -Keystore "C:\Users\petrj\AppData\Local\Xamarin\Mono for Android\KeyStore\PJsAndroidKeyStore\PJsAndroidKeyStore.keystore" `
+        -Alias "PJsAndroidKeyStore" `
+        -Password $passw 
+    
 
-# Sign aab
-
-$jarsigner = "C:\Program Files (x86)\Android\openjdk\jdk-17.0.14\bin\jarsigner.exe"
-$aab = "DVBTTelevizor.MAUI\bin\Release\net9.0-android35.0\net.petrjanousek.DVBTTelevizor.aab"
-$keystore = "C:\Users\petrj\AppData\Local\Xamarin\Mono for Android\KeyStore\PJsAndroidKeyStore\PJsAndroidKeyStore.keystore"
-$keystoreAlias = "PJsAndroidKeyStore"
-
-Write-Host "Signing the package"
-
-& $jarsigner -keystore $keystore  -storepass $passw $aab $keystoreAlias
-
-Write-Host "Signing the package........."
-
-# Check AndroidTV
-
-[xml]$manifest = Get-Content ".\DVBTTelevizor.MAUI\Platforms\Android\AndroidManifest.xml"
-if ($manifest.manifest.'uses-feature'.name -eq  "android.software.leanback")
-{
-    $suffix = ".AndroidTV"
-} else
-{
-    $suffix = ""
-}
-
-# Check Version
-
-[xml]$csproj = Get-Content ".\DVBTTelevizor.MAUI\DVBTTelevizor.MAUI.csproj"
-$version = $csproj.Project.PropertyGroup.VersionCode
-if ($version -is [System.Array])
-{
-    $version = $version[0]
-}
-
-$aabName = "net.petrjanousek.DVBTTelevizor.${version}${suffix}.aab"
-
-Copy-Item $aab $aabName
-
-# Create APK
-
-    # https://github.com/google/bundletool/releases
-    # "C:\Program Files (x86)\Android\android-sdk\platform-tools\bundletool-all-1.18.1.jar"
-
-$bundleTool = "C:\Program Files (x86)\Android\android-sdk\platform-tools\bundletool-all-1.18.1.jar"
-$java = "C:\Program Files (x86)\Android\openjdk\jdk-17.0.14\bin\java.exe"
-
-$outputArchive = Join-Path -Path $PSScriptRoot -ChildPath "$aabName.apks"
-
-if (Test-Path -Path $outputArchive)
-{
-    Remove-Item -Path $outputArchive -Force -Verbose
-} 
-
-Write-Host "Creating universal APK"
-
-& $java -jar $bundleTool build-apks --bundle=$aabName --output=$outputArchive --mode=universal --ks=$keystore --ks-key-alias=$keystoreAlias --ks-pass=pass:$passw 
-
-Rename-Item -Path $outputArchive -NewName ($outputArchive + ".zip")
-$outputArchive+=".zip"
-
-$apkName =  [System.IO.Path]::GetFileNameWithoutExtension($aabName) + ".apk"
-
-Expand-Archive -Path $outputArchive -DestinationPath . -Verbose
-
-Remove-Item "*.pb"
-Remove-Item $outputArchive
-Rename-Item "universal.apk" $apkName
-
-Get-Item $apkName
+$signedAABPackage | Copy-Item -Destination . -Force -Verbose
+$signedAPKPackage | Copy-Item -Destination . -Force -Verbose
+   
