933b8798905fcddea87d1430b33e7ec358d4ddb6
petr.janousek <petr.janousek@zat.cz>
2025-09-02 09:47:10 +0200

    linux build modifications

--- MAUI-Build-Linux.ps1
+++ MAUI-Build-Linux.ps1
@@ -1,3 +1,12 @@
+Set-Location $PSScriptRoot
+
 ./Clear.ps1
-dotnet publish --framework "net9.0-android35.0" /p:AndroidSdkDirectory=/home/kirk/Android/Sdk/ /p:AndroidPackageFormat=apk /t:Build DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj 
+
+$env:JAVA_HOME = "/usr/lib/jvm/java-17-openjdk-amd64"
+$env:PATH = "${$env:JAVA_HOME}/bin:$env:PATH"
+
+$env:ANDROID_HOME = "$HOME/Android"
+$env:PATH = "$env:ANDROID_HOME/bin:$env:PATH"
+
+dotnet publish --framework "net9.0-android35.0" /p:AndroidSdkDirectory=$HOME/Android/Sdk/ /p:AndroidPackageFormat=aab /t:Build DVBTTelevizor.MAUI/DVBTTelevizor.MAUI.csproj
 
