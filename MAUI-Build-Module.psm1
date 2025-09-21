fd3ad77f25e47b124a7169ccfcd7243b3f376c77
petrj <petrjanousek.net@gmail.com>
2025-09-21 11:38:46 +0200

    ver 2025007

--- MAUI-Build-Module.psm1
+++ MAUI-Build-Module.psm1
@@ -36,7 +36,7 @@ function Publish-AABPackage
         [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [System.IO.FileInfo]$InputObject,
 
-        [string]$Framework     = "net9.0-android35.0",
+        [string]$Framework     = "net9.0-android",
 
         [ValidateSet("Debug", "Release")]
         [string]$Configuration = "Release",
