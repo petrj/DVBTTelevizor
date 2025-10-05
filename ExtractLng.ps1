9d994133635a076efc684cbb21303057fc2b199a
petrj <petrjanousek.net@gmail.com>
2025-10-05 16:33:26 +0200

    ExtractLng.ps1

--- ExtractLng.ps1
+++ ExtractLng.ps1
@@ -2,13 +2,75 @@
 
 $files = Get-ChildItem -Path .\DVBTTelevizor.MAUI -Recurse | where { $_.Name.EndsWith(".cs") -or $_.Name.EndsWith(".xaml") }
 
+
+function Get-TranslatedText 
+{
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Line
+    )
+
+  if ($Line -match '"((?:[^"\\]|\\.)+)"\s*\.Translated\s*\(') {
+        # Unescape \" -> "
+        return ($matches[1] -replace '\\\"', '"')
+        }
+}
+
+
+function Get-XamlTranslatedText {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Line
+    )
+
+    process {
+       if ([String]::IsNullOrWhiteSpace($Line))
+       {
+        return $null
+       }
+       
+    }
+}
+
+
+$dict = @()
 foreach ($f in $files)
 {
     foreach ($line in Get-Content -Path $f.FullName)
     {
-        if ($line.Contains("Translated()"))
+        # searching  "text".Translated()
+        if ($line.Contains("Translated("))
         {
-            $line.Split(",")
+                foreach ($l in $line.Split(","))
+                {
+                    if ([String]::IsNullOrWhiteSpace($l))
+                    {
+                        continue
+                    }
+
+                    $txt = $l | Get-TranslatedText
+                    if (-not $dict.Contains($txt))
+                    {
+                        $dict+= $txt
+                    }
+                }            
+
         }
+
+
+        # searching Text="{local:LngXamlExt Input='No channel'}"
+        if ($line.Contains("LngXamlExt"))
+        {
+            foreach ($l in $line.Split(" "))
+            {
+                $l | Get-XamlTranslatedText
+            }
+        }
+
     }
 }
+
+
+
+#$dict | Out-GridView
