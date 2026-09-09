2d4a576a1a56b313a063bb5364563c5355de3326
PetrJ <petrjanousek.net@gmail.com>
2026-09-09 21:57:01 +0200

    Sonar script

--- ScanBySonarQube.ps1
+++ ScanBySonarQube.ps1
@@ -3,7 +3,9 @@
 ./LoadBuildModule.ps1
 
 $token = Get-SecureStringFromUserInput -Message "Enter SonarQube token:" -EnvironmentVariable $env:SONAR_TOKEN
-$key = Get-SecureStringFromUserInput -Message "Enter SonarQube project key:" -EnvironmentVariable $env:SONAR_KEY_DVBTTELEVIZOR
 $url = Get-SecureStringFromUserInput -Message "Enter SonarQube project url:" -EnvironmentVariable $env:SONAR_URL
 
-Invoke-SonarAnalysis -Token $token -ProjectKey $key -Url $url -WorkingDirectory  $PSScriptRoot
+Write-Host "dir: $PSScriptRoot"
+
+
+Invoke-SonarAnalysis -Token $token -ProjectKey "DVBTTelevizor" -Url $url -WorkingDirectory  $PSScriptRoot
