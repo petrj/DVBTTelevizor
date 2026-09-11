e3f7fedcdbc8f75012a4b03cb9c5604097a9c29b
PetrJ <petrjanousek.net@gmail.com>
2026-09-11 18:28:13 +0200

    Sonar script

--- ScanBySonarQube.ps1
+++ ScanBySonarQube.ps1
@@ -11,4 +11,9 @@ Write-Host "dir: $PSScriptRoot"
 Invoke-SonarAnalysis -Token $token -ProjectKey "DVBTTelevizor" -Url $url -WorkingDirectory  $PSScriptRoot
 
 
-Export-SonarQubeAnalysis -ProjectKey "DVBTTelevizor" -ServerUrl $env:SONAR_URL -Token $env:SONAR_TOKEN 
+#Export-SonarQubeAnalysis -ProjectKey "DVBTTelevizor" -ServerUrl $env:SONAR_URL -Token $env:SONAR_TOKEN 
+
+#$json = Get-Content -Path "SonarQubeAnalysis.json" -Raw | ConvertFrom-Json
+#$json | Where-Object { $_.Severity -eq "CRITICAL" } | ConvertTo-Json -Depth 10 | Set-Content -Path "SonarQubeAnalysis.Critical.json"
+
+#$json | Where-Object { ($_.Message -notlike "*Refactor this method to reduce its Cognitive Complexity*") -and ($_.Severity -eq "CRITICAL") } | ConvertTo-Json -Depth 10 | Set-Content -Path "SonarQubeAnalysis.Critical.json"
